a914ef3eeddb3333daba0e9bd88a999d81eaa35b
soupcan <greg@soupcan.org>
2020-09-12 23:35:27 -0400

    Initial commit

---
+++ FirstAI.cs
@@ -0,0 +1,109 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////
+//This Source Code File Is Part Of An
+//AI DLL Assembly for 
+//Empire Deluxe Combined Edition
+//
+//Copyright 2017 Mark Kinkead
+//May be freely hacked and altered to make your own 
+//non-commercial AI DLLs for Empire Deluxe Combined Edition
+//All other rights reserved
+//
+//This code can be hacked to to build AI players
+//Please remeber to create a unique dll name for your player
+//in order to be recognized by the game
+//This code can only be worked in association with 
+//the game Empire Deluxe Combined Edition
+//
+//Version Release Information Available
+//In the Factory file of each AI player
+//
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+//This is the implementation of the AI Factory for the Example AI.
+//It should not be within any namespace in order to be
+//recognized by the game program.
+
+using System.Collections.Generic;
+using com.kbs.empire.ai.common.player;
+using com.kbs.empire.ai.common.proc;
+using com.kbs.empire.common.game.data;
+using com.kbs.empire.common.game.hints;
+using com.kbs.empire.common.util.random;
+using com.kbs.empire.common.util.xml;
+using Soupcan.EmpireAI;
+
+//VERSION 1 09-09-17
+public class FirstAI : AIPlayerFactory
+{
+    ////////////////////////////////////////////
+    //For Individual Name Selection
+    private static readonly string[] leaders_ =
+        {
+            "Example"
+        };
+
+    private readonly List<string> aiGroup_ = new List<string>();
+    private readonly CMTRandom random_ = new CMTRandom();
+    ////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////
+    //The returned list gives the Unit Set DB keys
+    //that the AI is most attuned to. This can 
+    //include strings representing the keys for modified databases
+    public override List<string> bestBuildSets()
+    {
+        var ret = new List<string>();
+        ret.Add(EmpireCC.US_BS);
+        ret.Add(EmpireCC.US_SS);
+        ret.Add(EmpireCC.US_AS);
+        ret.Add(EmpireCC.US_ES);
+        return ret;
+    }
+
+    ////////////////////////////////////////////
+    //Hint Mechanism
+    //This allows the factory to serve up a new 
+    //hint set for the player to configure
+    public override CDLLHints getHints()
+    {
+        return FirstAIPlayer.getHints();
+    }
+
+
+    ////////////////////////////////////////////
+    //Creation Call 
+    //Returns a new instance of the player.
+    //note the pplayer name is also being set here
+    public override AIPlayer createAIPlayer(int position, string logpath, string logname, CDLLHints hints,
+        AIEventInterfaceI aiEvent, AICommandInterfaceI command, AIQueryI query, AICheatI cheat, int logLevel) 
+    {
+        //name selection
+        if (aiGroup_.Count == 0)
+        {
+            for (int i = 0; i < leaders_.Length; i++)
+                aiGroup_.Add(leaders_[i]);
+        }
+
+        int r = random_.nextInt(aiGroup_.Count);
+        string pname = aiGroup_[r];
+        aiGroup_.RemoveAt(r);
+
+        //instance creation
+        return new FirstAIPlayer(position, pname, logpath, logname, hints, aiEvent, command, query, cheat, logLevel);
+    }
+
+    ////////////////////////////////////////////
+    //Reload Call
+    //This call gives the data needed to reload an AI player instance
+    public override AIPlayer reloadAIPlayer(int position, CEncodedObjectInputBufferI bin, string logpath, string logname,
+        AIEventInterfaceI aiEvent, AICommandInterfaceI command, AIQueryI query, AICheatI cheat, int logLevel) 
+    {
+        Dictionary<string, string> caMap = bin.getAttributes();
+        return new FirstAIPlayer(position, caMap, bin, logpath, logname, aiEvent, command, query, cheat, logLevel);
+    }
+
+
+}
---
+++ com/kbs/empire/ai/example/FirstAIPlayer.cs
@@ -0,0 +1,198 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+//This Source Code File Is Part Of An
+//AI DLL Assembly for 
+//Empire Deluxe Combined Edition
+//
+//Copyright 2017 Mark Kinkead
+//May be freely hacked and altered to make your own 
+//non-commercial AI DLLs for Empire Deluxe Combined Edition
+//All other rights reserved
+//
+//This code can be hacked to to build AI players
+//Please remeber to create a unique dll name for your player
+//in order to be recognized by the game
+//This code can only be worked in association with 
+//the game Empire Deluxe Combined Edition
+//
+//Version Release Information Available
+//In the Factory file of each AI player
+//
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using com.kbs.empire.ai.common.cevent;
+using com.kbs.empire.ai.common.player;
+using com.kbs.empire.ai.common.proc;
+using com.kbs.empire.common.game.hints;
+using com.kbs.empire.common.util.log;
+using com.kbs.empire.common.util.xml;
+
+//See Factory For Version Info
+namespace Soupcan.EmpireAI
+{
+    //Example Player - the actual player instantce for this DLL
+    //This class takes advantage of AI player data, but that is not
+    //necessary. All AI PLayer Instances must ultiimately be derived 
+    //from the "AIPlayer" class found in the common library.
+    //It is recommended you review that class.
+    //And recommended you review AIPlayer Data if you care to use it (which is also recommended!)
+    //
+    public class FirstAIPlayer : AIPlayerData
+    {
+        //The output logger for debug work
+        public readonly CSubLog elogger_ = null;
+
+
+        //hints passed at construction
+        //These represent any hints the player has setup for this unique AI instance
+        private readonly CDLLHints hints_;
+
+        //construction
+        //This is the constructor for an AI Player at a new game.
+        public FirstAIPlayer(int position, string pname, string logpath, string logname, CDLLHints hints,
+            AIEventInterfaceI aiEvent, AICommandInterfaceI command, AIQueryI query, AICheatI cheat, int logLevel) 
+            : base(position, logpath, logname, aiEvent, command, query, cheat, logLevel)
+        {
+            //creation of the player log
+            elogger_ = new CSubLog("Soupcan First AI:" + Convert.ToString(position), realLog_);
+            elogger_.info("D Logger Log Open: " + logpath + " " + logname);
+            elogger_.info(pname + " waking up");
+
+            //copy hints
+            hints_ = hints.copy();
+
+            //copy assigned name
+            pname_ = pname;
+        }
+
+        //Run Turn
+        //The game gives the AI PLayer Thread control. 
+        //When it returns, the turn will be over
+        //the start turn event will be somewhere in the stack of events
+        //that the AI received. 
+        protected override void runTurn()
+        {
+            try
+            {
+                elogger_.info("Running Turn");
+
+                //pull all events since last we met
+                //This should end with the start turn
+                if (pollAllEvents()) { ackHold(); return; }
+
+                elogger_.info("Ending Turn");
+            }
+            catch (Exception T)
+            {
+                elogger_.info(T);
+            }
+        }
+
+        //Process Events
+        //override this method to do extra stuff with the events that come in
+        public override void processEvent(CGameEvent ge, CSubLog logger)
+        {
+            base.processEvent(ge, logger);
+
+            elogger_.info("Got Event:" + ge.type_);
+        }
+
+        //Saving The AI State
+        //Attribute data needs to be completely saved before the child elements are saved
+        //Read Up on how to use the CEncodedObjectOutputBufferI interface
+        private const string TEST_ATTR = "TA";
+
+        private int testAttribute_ = 12;
+        public override void encodeInternal(CEncodedObjectOutputBufferI output)
+        {
+            ////////////////////
+            //Attributes First
+            encodeAttr(output);
+
+            //now write out your attributes
+            output.addAttr(TEST_ATTR, Convert.ToString(testAttribute_));
+
+            ////////////////////
+            //Derived Children Second
+            encodeChildren(output);
+
+            //now write out your objects
+            hints_.encode(output);
+
+        }
+
+        //Loading The AI State
+        //The map holds the attributes previously saved
+        //child elements must be loaded in the order they were saved
+        //however, note that any STATE (cities, units, etc.) data used in the parent classes 
+        //(AIPlayer and AIPlayerData) will not be set until aiResotred is called.
+        //Read Up on how to use the CEncodedObjectInputBufferI interface
+        //
+        //When the Input Buffer enters this function, the "xml tape" is to be assumed to 
+        //be at the first element written out i encode enternal.
+        //caMap contains any attributes written out in EncodeInternal
+        //
+        public FirstAIPlayer(
+            int position, 
+            Dictionary<string, string> caMap, 
+            CEncodedObjectInputBufferI bin, 
+            string logpath, 
+            string logname,
+            AIEventInterfaceI aiEvent, 
+            AICommandInterfaceI command, 
+            AIQueryI query, 
+            AICheatI cheat,
+            int logLevel) 
+            : base(
+                position, 
+                logpath, 
+                logname, 
+                caMap,
+                bin, 
+                aiEvent, 
+                command, 
+                query, 
+                cheat,
+                logLevel)
+        {
+            elogger_ = new CSubLog("FirstAIPlayer:" + Convert.ToString(position), realLog_);
+            elogger_.info("D Logger Log Open: " + logpath + " " + logname);
+            elogger_.info("Position " + Convert.ToSingle(position) + " waking up.");
+
+            //get attributes
+            testAttribute_ = EncodeUtil.parseInt(caMap[TEST_ATTR]);
+
+            //get objects - must retrieve in the order they were submitted
+            hints_ = new CDLLHints(bin);
+
+
+            
+        }
+
+        //Post reload from save activities, prior to game play
+        //the base call reloads the state data into AIPlayerData (if in fact derived), 
+        //then the AI player can reconnect any direct
+        //object references used in the derived objects
+        public override void aiRestored(CStateEvent cse)
+        {
+            elogger_.info("Restored started");
+            base.aiRestored(cse);
+            elogger_.info("Restored done");
+            elogger_.info(pname_ + " is now self aware.");
+        }
+
+        //Creation of Hints
+        //indicate the hints the player can touch and set their defaults
+        private const string DO_NOTHING_ATTR = "DN";
+        public static CDLLHints getHints()
+        {
+            //Key Must Not Contain Spaces
+            var ret = new CDLLHints(new CDLLInfo("SoupcanFirstAI", "Soupcan First AI", "Example Stub For AI...not too challenging", "1.0"));
+            ret.addInfo(new CDLLBoolHintInfo(DO_NOTHING_ATTR, "Do nothing", "Do Not Expand", false));
+            return ret;
+        }
+
+    }
+}
